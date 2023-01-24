740f7d6c278ba69d93f2c5f39ca47e0cd38f5605
Maksym Lepikhov <Werzeragna@icloud.com>
2023-01-24 18:21:37 +0200

    JS Homework 7 v1.3

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,7 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galleryEl = document.querySelector(".galler");
+const galleryEl = document.querySelector(".gallery");
 
 const createImages = (options) =>
   options
